007e2d2e74cc7d12759bd4f113168187b319f857
Daniel <danieldiasbarbo@gmail.com>
2022-01-30 11:14:54 -0300

    database connection

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const customExpress = require('./config/customExpress')
+const connection = require('./infraestrutura/connection')
 
-const app = customExpress()
+connection.connect(error => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log('connection successful')
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
