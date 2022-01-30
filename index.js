41a7ffeed6396370d69580d0d2fb99c7008d2779
Daniel <danieldiasbarbo@gmail.com>
2022-01-30 14:48:21 -0300

    Inserting data to table

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customExpress = require('./config/customExpress')
 const connection = require('./infraestrutura/connection')
+const Tables = require('./infraestrutura/tables')
 
 connection.connect(error => {
     if(error) {
@@ -7,6 +8,7 @@ connection.connect(error => {
     } else {
         console.log('connection successful')
 
+        Tables.init(connection)
         const app = customExpress()
 
         app.listen(3000, () => console.log('servidor rodando na porta 3000'))
